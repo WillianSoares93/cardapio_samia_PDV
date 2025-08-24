e2a97965f4a52de648e01c8204d3a5177d5db65d
WillianSoares93 <cwsm1993@gmail.com>
2025-08-24 16:49:56 -0300

    Update criar-pedido.js

--- api/criar-pedido.js
+++ api/criar-pedido.js
@@ -40,8 +40,8 @@ export default async (req, res) => {
         let pdvError = null;
         try {
             // Simulação de erro ao salvar no Firestore (para testes)
-            console.error('[TEST] Simulando erro no Firestore: SIMULATED_FIRESTORE_ERROR');
-            throw new Error('SIMULATED_FIRESTORE_ERROR');
+            //console.error('[TEST] Simulando erro no Firestore: SIMULATED_FIRESTORE_ERROR');
+           //throw new Error('SIMULATED_FIRESTORE_ERROR');
             await addDoc(collection(db, "pedidos"), {
                 itens: order,
                 endereco: selectedAddress,
