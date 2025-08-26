31051b50fa5ae3ccb3cabc0154df29b78b2d2a6f
WillianSoares93 <cwsm1993@gmail.com>
2025-08-25 23:55:26 -0300

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -25,12 +25,15 @@ if (!getApps().length) {
 const db = getFirestore(app);
 
 
+
+
+
 // URLs das suas planhas Google Sheets publicadas como CSV.
-const CARDAPIO_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDOVxav8wiNNTL5Jt1Cq3F3fBD7yLyHLGi2c_0r1abHVZRY2W7RbW3h4FX_poEdCXE8Tl85KGAU28v/pubhtml?gid=1575270352&single=true'; 
-const PROMOCOES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDOVxav8wiNNTL5Jt1Cq3F3fBD7yLyHLGi2c_0r1abHVZRY2W7RbW3h4FX_poEdCXE8Tl85KGAU28v/pubhtml?gid=1622604495&single=true'; 
-const DELIVERY_FEES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDOVxav8wiNNTL5Jt1Cq3F3fBD7yLyHLGi2c_0r1abHVZRY2W7RbW3h4FX_poEdCXE8Tl85KGAU28v/pubhtml?gid=679334079&single=true';
-const INGREDIENTES_HAMBURGUER_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDOVxav8wiNNTL5Jt1Cq3F3fBD7yLyHLGi2c_0r1abHVZRY2W7RbW3h4FX_poEdCXE8Tl85KGAU28v/pubhtml?gid=1298581759&single=true';
-const CONTACT_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDOVxav8wiNNTL5Jt1Cq3F3fBD7yLyHLGi2c_0r1abHVZRY2W7RbW3h4FX_poEdCXE8Tl85KGAU28v/pubhtml?gid=1022597597&single=true';
+const CARDAPIO_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RERYG8TDuibOadfLJAHAoc8I64hMrLkDmoIcnVOdJZ0/export?format=csv&gid=1575270352'; 
+const PROMOCOES_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RERYG8TDuibOadfLJAHAoc8I64hMrLkDmoIcnVOdJZ0/export?format=csv&gid=1622604495'; 
+const DELIVERY_FEES_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RERYG8TDuibOadfLJAHAoc8I64hMrLkDmoIcnVOdJZ0/export?format=csv&gid=1298581759';
+const INGREDIENTES_HAMBURGUER_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RERYG8TDuibOadfLJAHAoc8I64hMrLkDmoIcnVOdJZ0/export?format=csv&gid=679334079';
+const CONTACT_CSV_URL = 'https://docs.google.com/spreadsheets/d/1RERYG8TDuibOadfLJAHAoc8I64hMrLkDmoIcnVOdJZ0/export?format=csv&gid=1022597597';
 
 // Leitor de linha CSV robusto que lida com vírgulas dentro de aspas
 function parseCsvLine(line) {
